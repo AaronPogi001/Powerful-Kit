2ae7cd59e31485cca75dd5d3bee3f99d049ded48
Aaron <2021-101141@rtu.edu.ph>
2024-10-23 16:46:02 +0800

    10/23-WFH

--- assets/js/main.js
+++ assets/js/main.js
@@ -224,13 +224,11 @@ document.querySelectorAll('.readMoreBtn').forEach(button => {
     const shortDescription = memberInfo.querySelector('.shortDescription');
 
     if (fullDescription.style.display === 'none') {
-      fullDescription.style.display = 'block'; // Show full description
-      shortDescription.style.display = 'none'; // Hide short description
-      this.textContent = 'Read Less'; // Change button text
+      fullDescription.style.display = 'block'; 
+      shortDescription.style.display = 'none'; 
     } else {
-      fullDescription.style.display = 'none'; // Hide full description
-      shortDescription.style.display = 'block'; // Show short description
-      this.textContent = 'Read More'; // Change button text
+      fullDescription.style.display = 'none'; 
+      shortDescription.style.display = 'block'; 
     }
   });
 });
