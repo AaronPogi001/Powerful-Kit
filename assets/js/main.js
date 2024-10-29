89542e328c369fe1259dc37825965ab5df0b6410
Aaron <2021-101141@rtu.edu.ph>
2024-10-29 13:46:20 +0800

    10/26

--- assets/js/main.js
+++ assets/js/main.js
@@ -233,3 +233,6 @@ document.querySelectorAll('.readMoreBtn').forEach(button => {
   });
 });
 
+const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
+const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
+
